03f8f541b5dab7ecbcdcaa2b94bd784d75088d93
yuri97 <yuri97real@gmail.com>
2021-12-19 18:09:08 -0400

    Refactoring to Class Format

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # Validador de CPF
 
-Esse script valida números de cpf, independente de haver caracteres numéricos ou não, ou seja, funciona com ou sem máscara de input.
-Ex:. Assim "12345678900" ou assim "123.456.789-00"
-Mas se for usar uma máscara, eu também desenvolvi uma. Dá uma conferida, https://github.com/yuri97real/FormCPF
-Obs:. A receita federal pode mudar o cálculo usado para validação, isso já aconteceu. Portanto, ao implementar esse código em seu projeto, minha sugestão é que mostre apenas um lembrete amigável para o usuário. Não o impeça de fazer um cadastro mesmo que o script acuse erro. 
-Ps. Já aconteceu comigo, de um sistema antigo negar meu cpf.
+## Descrição
+
+Este projeto é um validador de CPF's, desenvolvido em JavaScript.
+
+## Como usar?
+
+1. Importe o arquivo "validateCpf.js" no seu HTML.
+2. Instancie a classe ValidateCPF, passando seu elemento input como parâmetro no construtor.
+
+## Exemplo
+
+No arquivo HTML
+
+    <script src="validateCpf.js"></script>
+
+No arquivo JS
+
+    const cpf = document.querySelector("#cpf")
+    new ValidateCPF(cpf)
+
+## Features
+
+- [x] Validar apenas os números do input
+- [x] Verificar números repetidos
+
+Dá uma conferida, na <a href="https://github.com/yuri97real/FormCPF">Máscara de Inputs</a> que eu desenvolvi.
+
+## Observações
+
+A receita federal pode mudar o cálculo usado para validação, isso já aconteceu. 
+
+Portanto, ao implementar esse código em seu projeto, minha sugestão é que mostre apenas um lembrete amigável para o usuário. Não o impeça de fazer cadastro mesmo que o script acuse erro.
+
+1. Já aconteceu comigo, de um sistema antigo negar meu cpf.
+2. Não esqueça de validar o cpf também no back-end.
--- validateCpf.js
+++ validateCpf.js
@@ -1,58 +1,60 @@
-const cpf = document.querySelector("#cpf")
-const helper = document.querySelector(".help-box-result")
+class ValidateCPF {
 
-cpf.addEventListener("keyup", e => {
+    constructor(cpf) {
 
-    const vetCpf = cpf.value.split("")
-    const onlyNumbers = vetCpf.filter(value => !isNaN(value))
-    const isComplete = onlyNumbers.length >= 11
+        this.cpf = cpf
+        this.cpf.addEventListener("keyup", () => this.validate())
 
-    if(!isComplete) return
-
-    const firstNineNumbers = onlyNumbers.slice(0, -2)
-
-    const removeDuplicates = onlyNumbers.filter((value, pos) => onlyNumbers.indexOf(value) == pos)
-    const areAllTheSame = removeDuplicates.length == 1
-
-    if(!areAllTheSame) {
-        
-        const firstCheckDigit = getCheckDigit(firstNineNumbers, 10)
-        firstNineNumbers.push(firstCheckDigit)
-        const secondCheckDigit = getCheckDigit(firstNineNumbers, 11)
-        firstNineNumbers.push(secondCheckDigit)
-
-        validateCpf(firstNineNumbers, onlyNumbers);
-
-    } else {
-        showMessageResult("INVÁLIDO, NÚMEROS IGUAIS", ["invalid"])
     }
 
-})
+    validate() {
 
-function getCheckDigit(firstNineNumbers, subtr) {
+        const cpfChars = this.cpf.value.split("")
+        const onlyNumbers = cpfChars.filter(value => !isNaN(value))
+        const isComplete = onlyNumbers.length >= 11
 
-    const multiplyCpfNumbersWithVector = firstNineNumbers.map((value, pos) => value * (subtr - pos))
-    const sumNumbers = multiplyCpfNumbersWithVector.reduce((value, acc) => value + acc, 0)
-    const restOfDivisionByEleven = sumNumbers % 11
-    const checkDigit = (restOfDivisionByEleven < 2) ? 0 : (11 - restOfDivisionByEleven)
+        if(!isComplete) return
 
-    return checkDigit
+        const firstNineNumbers = onlyNumbers.slice(0, -2)
 
-}
+        const removeDuplicates = onlyNumbers.filter((value, pos) => onlyNumbers.indexOf(value) == pos)
+        const areAllTheSame = removeDuplicates.length == 1
 
-function validateCpf(validCpf, cpfInformed) {
+        if(areAllTheSame) return this.showMessage("INVÁLIDO, NÚMEROS IGUAIS", ["invalid"])
+            
+        const firstCheckDigit = this.getCheckDigit(firstNineNumbers, 10)
+        firstNineNumbers.push(firstCheckDigit)
+        const secondCheckDigit = this.getCheckDigit(firstNineNumbers, 11)
+        firstNineNumbers.push(secondCheckDigit)
 
-    validCpf.join("") === cpfInformed.join("") ?
-        showMessageResult("CPF VÁLIDO", ["valid"]) : showMessageResult("CPF INVÁLIDO", ["invalid"])
+        const verifiedCPF = firstNineNumbers.join("")
+        const cpfInformed = onlyNumbers.join("")
 
-}
+        verifiedCPF === cpfInformed ?
+            this.showMessage("CPF VÁLIDO", ["valid"]) :
+            this.showMessage("CPF INVÁLIDO", ["invalid"])
 
-function showMessageResult(msg, classes) {
+    }
 
-    cpf.classList.remove("valid", "invalid")
+    getCheckDigit(firstNineNumbers, subtr) {
 
-    helper.innerText = msg
+        const multiplyCpfNumbersWithVector = firstNineNumbers.map((value, pos) => value * (subtr - pos))
+        const sumNumbers = multiplyCpfNumbersWithVector.reduce((value, acc) => value + acc, 0)
+        const restOfDivisionByEleven = sumNumbers % 11
+        const checkDigit = (restOfDivisionByEleven < 2) ? 0 : (11 - restOfDivisionByEleven)
+    
+        return checkDigit
+    
+    }
+    
+    showMessage(msg, classes) {
+    
+        cpf.classList.remove("valid", "invalid")
+        classes.forEach(className => cpf.classList.add(className))
 
-    classes.forEach(className => cpf.classList.add(className))
+        console.clear()
+        console.log(msg)
+    
+    }
 
 }
